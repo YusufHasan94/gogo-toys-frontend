2e822b02063dffb44abb8c890995c57413760c0a
YusufHasan94 <yhpolok@gmail.com>
2025-02-09 19:09:17 +0600

    adding hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/home/Hero";
 import Header from "@/shared/Header";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/home/Hero/index.tsx
@@ -0,0 +1,18 @@
+import hero from "@/assets/home/hero.png"
+import Image from "next/image";
+const Hero = () => {
+    return (
+        <div className="relative">
+            <Image src={hero} alt="hero" className="relative w-full"/>
+            <div className="absolute top-1/4 right-[120px]">
+                <h1 className="text-6xl text-[#1096B5] font-extrabold">Play, learn, & grow!</h1>
+                <p className="mt-4 mb-16 text-[#375259] text-xl font-bold w-[545px]">Crafting smiles with every toy, made for learning, fun, and growth</p>
+                <button className="bg-[#FFE926] rounded-[74px] px-11 py-5 text-xl font-bold">
+                    Shop now 
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
